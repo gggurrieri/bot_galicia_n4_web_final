fc923f382a23d2936397c27247e5a64860710a0c
gggurrieri <L0293857@bancogalicia.com.ar>
2025-05-22 16:38:34 -0300

    Fix: import correcto y puerto ajustado para Render

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def index():
     return render_template("index.html", resumen=resumen, status=status)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
